8e3c53b6616707bbaaabaf273def75950dda0ed7
Dario Spadola <cliffrobinson3@gmail.com>
2025-11-18 22:00:14 +0100

    Add files via upload

---
+++ 02_voto if else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    int voto;
+    voto = 6;
+    if (voto >= 6) {
+    	printf ("Promosso \n");
+	}else{
+		printf ("Bocciato \n");
+	}
+}
